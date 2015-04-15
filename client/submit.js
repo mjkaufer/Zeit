1199f291a7990cdbff14fbb82822e1160989e4e3
mjkaufer <mjkaufer@gmail.com>
2015-04-14 21:05:24 -0400

    Better previewing

--- client/submit.js
+++ client/submit.js
@@ -1,15 +1,35 @@
 Template.submit.helpers({
 	article: function(){
-		return Session.get("article");
+		return Session.get("article") != "\n" ? Session.get("article") : "\nArtikel Text";
 	},
 	title: function(){
-		return Session.get("title");
+		return Session.get("title") || "Titel";
+	},
+	options: function(){
+		return Newspapers.find({})
+	},
+	date: function(){
+		return Session.get("date");
 	}
-})
+
+});
 
 Template.submit.events({
 	'keyup': function(){
-		Session.set("article", "\n" + $('#article').val());
-		Session.set("title", $('#title').val());
+		updateSessionVars();
+	},
+	'mouseup': function(){
+		updateSessionVars();
 	}
-})
+});
+
+function updateSessionVars(){
+		Session.set("article", "\n" + $('#article').val());//need to concatenate the newline because of a bug in the markdown package
+		Session.set("title", $('#title').val());
+		Session.set("date", $('#issue').text().trim());
+
+}
+
+Template.submit.rendered = function(){
+	updateSessionVars();
+}
