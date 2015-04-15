3994dec27ba069a7bf3dc986fc98f40cbd00bfe0
mjkaufer <mjkaufer@gmail.com>
2015-04-15 19:01:19 -0400

    Fixed issue title

--- client/issue.js
+++ client/issue.js
@@ -1,6 +1,3 @@
-//because stuff is silly...
-var paperId = document.URL.substring(document.URL.lastIndexOf("/")+1, document.URL.length);
-
 Template.issue.helpers({
     articles: function() {
         return Posts.find({}, {
@@ -8,9 +5,5 @@ Template.issue.helpers({
                 submitted: -1
             }
         });
-    },
-    paperInfo: function(){
-    	var thisPaper = Newspapers.findOne(new Mongo.ObjectID(paperId));
-    	return thisPaper.month + " " + thisPaper.year;
     }
-})
+});
