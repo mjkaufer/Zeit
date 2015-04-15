e74b3e43926c82b7df049e47c9b1d1b269588b64
mjkaufer <mjkaufer@gmail.com>
2015-04-15 16:14:59 -0400

    Removed strange file

--- client/header.js
+++ client/header.js
@@ -4,14 +4,14 @@ Template.header.helpers({
     papers: function() {
         var ids = [];
 
-        Newspapers.find().forEach(function(a) {
+        Newspapers.find({},{sort:{year: -1,monthNum: -1}}).forEach(function(a) {
             ids.push({
                 id: a._id._str,
                 month: a.month,
                 year: a.year
             });
         });
-        
+
         return ids;
     }
 })
